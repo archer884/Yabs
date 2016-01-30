0d7d3fbb7c52b46e86aca86399014c02717529c6
GGYE <ggye@free.Fr>
2016-01-30 19:29:04 +0100

    Find files to commit (without taking timestamp into account)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate filetime;
 extern crate rustc_serialize;
 
 use std::path::Path;
+use std::collections::HashMap;
 
 
 mod metadata;
@@ -14,9 +15,35 @@ fn main() {
 	metadata::create_emty_metadata_file(json_path);
 
 	let data_path = Path::new("data");
-	let hierarchy = workingdirectory::read_working_directory(data_path);
+	let wd_hierarchy = workingdirectory::read_working_directory(data_path);
 
-	println!("{:?}", hierarchy);
+	let mt_hierarchy = metadata::read_metadata_file(json_path);
+
+	println!("{:?}", mt_hierarchy);
+
+	println!("Finding files to commit");
+
+	let files_to_commit = files_to_commit(wd_hierarchy, mt_hierarchy);
+
+	println!("Files to commit {:?}", files_to_commit);	
+}
+
+fn files_to_commit(wd_hierarchy: HashMap<String, model::MetaData>, mt_hierarchy: model::Hierarchy) -> Vec<String>  {
+	let mut files_to_commit: Vec<String> = Vec::new();
+
+	for (filename, metadata) in wd_hierarchy.iter() {
+		let actual_metadata = mt_hierarchy.get_latest_meta_data(&filename);
+		println!("actual metadata {:?}", actual_metadata);
+
+		match actual_metadata {
+				Some(x) => (),
+				None => files_to_commit.push(filename.clone())
+			}
+	    //files_to_commit.push(filename.clone());
+	}
+
+
+	files_to_commit
 }
 
 mod model {
@@ -47,6 +74,18 @@ mod model {
 			let empty_hierarchy_map : HashMap<String, MetaDataSet> = HashMap::new();
 			Hierarchy {nb_revision: 1, files: empty_hierarchy_map}
 		}	
+		pub fn get_latest_meta_data(&self, filename: &String) -> Option<&MetaData> {
+			match self.files.get(filename) {
+				Some(x) => x.get_last(),
+				None => None
+			}
+		}
+	}
+
+	impl MetaDataSet {
+		pub fn get_last(&self) -> Option<&MetaData> {
+			self.metadata.last()
+		}
 	}
 
 	impl MetaData {
--- src/metadata.rs
+++ src/metadata.rs
@@ -11,15 +11,15 @@ use model::Hierarchy;
 use model::MetaDataSet;
 
 pub fn create_emty_metadata_file<P: AsRef<Path>>(path: P) {
-	println!("Creating empty metadata file");  
+	println!("Creating empty metadata file in {}", path.as_ref().to_str().unwrap());  
 
 	let hierarchy = Hierarchy::new_empty();
 
-	println!("hey : {:?}", hierarchy);
+//	println!("hey : {:?}", hierarchy);
 
 	let json_hierarchy =   json::encode(&hierarchy).unwrap();
 
-	println!("hey in json: {:?}", json_hierarchy);    
+//	println!("hey in json: {:?}", json_hierarchy);    
 
 	let mut file = File::create(&path).unwrap();
 
@@ -30,4 +30,14 @@ pub fn create_emty_metadata_file<P: AsRef<Path>>(path: P) {
     file.sync_all();
 }
 
+pub fn read_metadata_file<P: AsRef<Path>>(path: P) -> Hierarchy{
+	println!("Reading metadata file from {}", path.as_ref().to_str().unwrap());  
+
+	let mut file = File::open(&path).unwrap();
+	let mut json = String::new();
+    file.read_to_string(&mut json);
+
+    let hierarchy: Hierarchy = json::decode(&json).unwrap();
+    hierarchy
+}
 
